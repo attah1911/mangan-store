4df2bc654c682efd8a9dd64d715e4afd769380bc
hatta1911 <hattagithub@gmail.com>
2024-11-15 15:26:08 +0700

    feat: admin page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
-import Navbar from "@/components/layouts/Navbar";
+import Navbar from "@/components/fragments/Navbar";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Alexandria } from "next/font/google";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const alexandria = Alexandria({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+const disableNavbar = ["auth", "admin"];
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const { pathname } = useRouter();
+  console.log(pathname.split("/")[1]);
+
   return (
     <SessionProvider session={session}>
       <Head>
@@ -23,7 +29,7 @@ export default function App({
         ></link>
       </Head>
       <div className={alexandria.className}>
-        <Navbar />
+        {!disableNavbar.includes(pathname.split("/")[1]) && <Navbar />}
         <Component {...pageProps} />
       </div>
     </SessionProvider>

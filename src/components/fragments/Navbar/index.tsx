1ba7d53dc1d2b539e8af3c2f4e671349aabf78dd
hatta1911 <hattagithub@gmail.com>
2024-11-22 13:14:15 +0700

    feat: member page v2

--- src/components/fragments/Navbar/index.tsx
+++ src/components/fragments/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./Navbar.module.scss";
 
+// test
 const Navbar = () => {
   const { data } = useSession();
   return (
